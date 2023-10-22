f767153a2e6b2816f3cb9f0db46e443b9b669174
KarolKowalewicz <kowalewiczkarol@gmail.com>
2023-10-22 13:42:14 +0200

    nav

---
+++ script.js
@@ -0,0 +1,29 @@
+// Pobierz wszystkie linki z menu
+const menuLinks = document.querySelectorAll(".menu li a");
+
+// Dodaj obsługę kliknięcia do każdego linku
+menuLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    // Usuń klasę "active" z obecnie aktywnego linku
+    const currentActiveLink = document.querySelector(".menu li a.active");
+    if (currentActiveLink) {
+      currentActiveLink.classList.remove("active");
+    }
+
+    // Dodaj klasę "active" do klikniętego linku
+    link.classList.add("active");
+  });
+});
+
+
+
+// // Pobieramy element z opisem
+// const textElement = document.getElementById("description");
+
+// // Tworzymy nowy opis z podziałem na nowe linie
+// const nevDescr = "Next Level\nMachine\ninteligence";
+
+// // Aktualizujemy treść opisu
+// textElement.textContent = nevDescr;
+
+
