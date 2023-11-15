529c985b1d29941d22262482348658e43a4287ef
Konrad Perko <konrad.perko@esab.se>
2023-11-15 22:17:44 +0100

    sugestie konrada

--- script.js
+++ script.js
@@ -16,9 +16,6 @@ menuLinks.forEach((link) => {
 });
 
 
-
-
-
 // // Pobieramy element z opisem
 // const textElement = document.getElementById("description");
 
@@ -27,5 +24,23 @@ menuLinks.forEach((link) => {
 
 // // Aktualizujemy treść opisu
 // textElement.textContent = nevDescr;
+
+
+
+/*
+1. eventLister na scroll:
+2. pobrac element nawigacji
+3. usunac klase navbar--top jesli pozycja scroll'a nie jest 0
+4. nadawac klase navbar--top jesli pozycja scroll'a jest 0
+*/
+
+const navbar = document.querySelector('.navbar');
+window.addEventListener('scroll', (ev) => {
+  if (window.scrollY > 65) {
+    navbar.classList.remove('navbar--top');
+  } else {
+    navbar.classList.add('navbar--top');
+  }
+});
 
 
